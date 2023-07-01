1f83590efb71fc3ffcf4e3165e94083b698f2db6
logesh <logesh.bhuvaneswaran@skillassure.com>
2023-07-01 11:00:01 +0530

    add two number

--- my-first-project/Program.cs
+++ my-first-project/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Schema;
 
 namespace my_first_project
 {
@@ -6,7 +7,13 @@ namespace my_first_project
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("enter first number:");
+            int sum1 =int.Parse(Console.ReadLine());
+            Console.Write("enter second number:");
+            int sum2 =int.Parse(Console.ReadLine());
+            int result  = sum1 + sum2;
+            string word = result.ToString();
+            Console.WriteLine("result is " +word );
         }
     }
 }
