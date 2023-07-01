ff4de9412dcff25488d7018ae8effc9529ddaea1
logusham <logusham1999@gmail.com>
2023-07-01 09:50:22 +0530

    creat new branch

--- my-first-project/Program.cs
+++ my-first-project/Program.cs
@@ -6,9 +6,7 @@ namespace my_first_project
     {
         static void Main(string[] args)
         {
-            Console.Write("What is your name : ");
-            string Name = Console.ReadLine();
-            Console.WriteLine("My name is "+Name);
+            
         }
     }
 }
