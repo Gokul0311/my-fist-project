f208371ff0f702d24f1f793173c46772592e976b
logesh <logesh.bhuvaneswaran@skillassure.com>
2023-07-01 09:34:52 +0530

    inital code simple program

--- my-first-project/Program.cs
+++ my-first-project/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace my_first_project
+﻿using System;
+
+namespace my_first_project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("What is your name : ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("My name is "+Name);
         }
     }
 }
