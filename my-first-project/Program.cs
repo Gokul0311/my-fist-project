c2d405002c3fe2eff78f383acb5282ca7aeab574
logusham <logusham1999@gmail.com>
2023-07-08 12:59:09 +0530

    create a new branch for if

--- my-first-project/Program.cs
+++ my-first-project/Program.cs
@@ -7,30 +7,7 @@ namespace my_first_project
     {
         static void Main(string[] args)
         {
-            Console.Write("enter three number:");
-            int num = int.Parse(Console.ReadLine ());
-            //Console.WriteLine(value / 100);
-            switch(num)
-            {
-                case<10:
-                    Console.WriteLine(num);
-                    break;
-                case<100:
-                    Console.WriteLine(num / 10);
-                    break;
-                case<1000:
-                    Console.WriteLine(num / 100);
-                    break;
-                case<10000:
-                    Console.WriteLine(num / 1000);
-                    break;
-                default:
-                    Console.WriteLine("enter below tenthousand");
-                    break;
-                
-          
-                    
-            }   
+                     
         }
     }
 }
